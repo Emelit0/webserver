21e2fc8d7060a4c4d8f40fea5cea952c8fca1e6e
Emelit0 <stollk811@gmail.com>
2023-05-22 23:57:51 +0200

    add database context

---
+++ webserver/Helpers/DataContext.cs
@@ -0,0 +1,20 @@
+namespace webserver.Helpers;
+
+using Microsoft.EntityFrameworkCore;
+
+public class DataContext : DbContext
+{
+    protected readonly IConfiguration Configuration;
+
+    public DataContext(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        // connect to postgres with connection string from app settings
+        options.UseNpgsql(Configuration.GetConnectionString("WebApiDatabase"));
+    }
+
+}
