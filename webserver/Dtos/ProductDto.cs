291711795b4eea08ba5996f703026d3157093640
Emelit0 <stollk811@gmail.com>
2023-05-26 23:47:36 +0200

    add product dto

---
+++ webserver/Dtos/ProductDto.cs
@@ -0,0 +1,22 @@
+namespace webserver.Dtos;
+
+public class ProductDto
+{
+   
+   public int Id { get; set; }
+   
+   public string Name { get; set; } = null!;
+   
+   public string Description { get; set; } = null!;
+   
+   public decimal Price { get; set; }
+   
+   public int CategoryId { get; set; }
+   
+   public int SupplierId { get; set; }
+   
+   public virtual ICollection<UserDto> Users { get; set; } = null!;
+   
+   
+
+}
