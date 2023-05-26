704f40df296345b6545d9355f7aa4eafc216ed2a
Emelit0 <stollk811@gmail.com>
2023-05-26 23:49:03 +0200

    update pipeline

--- webserver/Program.cs
+++ webserver/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using webserver.Helpers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +10,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddEntityFrameworkNpgsql().AddDbContext<ApiDbContext>(opt =>
+builder.Services.AddEntityFrameworkNpgsql().AddDbContext<DataContext>(opt =>
     opt.UseNpgsql(builder.Configuration.GetConnectionString("WebApiDatabase")));
 
 var app = builder.Build();
