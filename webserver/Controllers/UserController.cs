dc4c0c8026410333c50081e3c76474b64f22c0b5
Emelit0 <stollk811@gmail.com>
2023-06-03 23:43:38 +0200

    add user controller

---
+++ webserver/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using webserver.Models;
+
+namespace webserver.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    private readonly ILogger<UserController> _logger;
+    private static readonly int[] Users = { 1, 2, 3, 4, 5 };
+
+    public UserController(ILogger<UserController> logger)
+    {
+        _logger = logger;
+    }
+
+    //get all users
+
+    [HttpGet (Name = "users")]
+    
+    public Task<ActionResult<IEnumerable<User>>> GetUsers()
+    {
+        return Task.FromResult<ActionResult<IEnumerable<User>>>(Ok(Users));
+    }
+}
